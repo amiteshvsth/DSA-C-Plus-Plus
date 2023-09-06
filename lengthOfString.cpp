39fa36d1b78d53d5fbc5cbfc7df8d99ddd72c5e0
Amitesh <amiteshvashishth@gmail.com>
2023-09-06 16:54:04 +0530

    Added a program to find length of string

---
+++ lengthOfString.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int getLength(char ch[]){
+    int count = 0;
+    for (int i = 0; ch[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+int main(){
+    char name[20];
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Your name is: " << name << endl;
+    cout << "Length: " << getLength(name);
+}
