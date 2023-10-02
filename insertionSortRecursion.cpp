6ba3a4ab58178eacc2da92a45f37e4ee9b1e3790
Amitesh <amiteshvashishth@gmail.com>
2023-10-02 21:41:05 +0530

    Added Comments

--- insertionSortRecursion.cpp
+++ insertionSortRecursion.cpp
@@ -3,13 +3,18 @@ using namespace std;
 void insertionSort(int *arr, int N){
     int i,key,j;
     for (i = 1; i < N; i++){
+        //if size of array is less than or equal to 1 just keep it as it is
         if(N<=1) return;
 
+        //otherwise calling insertionSort for n-1
         insertionSort(arr,N-1);
         int last =arr[N-1];
         int j=N-2;
+        //traversing from backwards comparing 2nd last element with last element
+        // if found big 
         while (j>=0 && arr[j]>last)
         {
+            //swapping the values
             arr[j+1]=arr[j];
             j--;
         }
--- selectionSortRecursion.cpp
+++ selectionSortRecursion.cpp
@@ -5,12 +5,20 @@ using namespace std;
 int mini(int *a, int i, int j){
     if(i==j) return i;
     int k =mini(a, i+1,j);
+    // Return minimum of current and remaining.
     return (a[i]<a[k])?i:k;
 }
 void selectionSort(int *arr, int n, int idx=0){
+    // Return when starting and size are same
     if(idx==n) return;
+
+    // calling mini function for minimum index
     int k= mini(arr,idx,n-1);
+
+    // Swapping when index and minimum index are not same
     if(k!=idx) swap(arr[k],arr[idx]);
+
+    // Recursively calling selection sort function
     selectionSort(arr,n, idx+1);
     
 }
@@ -19,7 +27,6 @@ int main(){
     int arr[] = { 64, 25, 12, 22, 11 };
     int n = sizeof(arr) / sizeof(arr[0]);
  
-    // Function Call
     selectionSort(arr, n);
     cout << "Sorted array: \n";
     for (int i = 0; i < n; i++) {
